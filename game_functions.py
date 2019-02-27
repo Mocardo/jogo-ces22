0c34b1a0ae7ec0e4f0e3c4d831dc164e266920f1
mouradfelipe <felpsmp1999@gmail.com>
2019-02-27 16:49:50 -0300

    ship and game functions

--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,25 @@
-def check_keydown_events(event, ai_setting, screen, ship, bullets):
+import sys
+
+import pygame
+
+def check_events():
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+
+
+
+
+
+
+#def check_keydown_events(event, ai_setting, screen, ship, bullets):
 
 
     #escreva sua parte aqui!! Mourad
 
 
 
-    elif event.key == pygame.K_q:
+    #elif event.key == pygame.K_q:
