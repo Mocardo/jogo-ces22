28cadbc4fe10804c286e5909c1f392b6a8f00627
KChenDu <kelvenchendu@gmail.com>
2019-02-27 16:26:46 -0300

    criei botao de saida

--- game_functions.py
+++ game_functions.py
@@ -4,5 +4,6 @@ def check_keydown_events(event, ai_setting, screen, ship, bullets):
     #escreva sua parte aqui!! Mourad
 
 
-
+    #criou-se botão de saída do jogo 'q', facilita...
     elif event.key == pygame.K_q:
+        sys.exit()
