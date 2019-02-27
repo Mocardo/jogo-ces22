1cc423aa7ce13dd0a62c5d14ecd484bb1c1497c2
mouradfelipe <felpsmp1999@gmail.com>
2019-02-27 17:44:34 -0300

    bullets

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.sprite import Group
+
 
 from settings import Settings
 from ship import Ship
@@ -14,16 +16,17 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
 run_game()
+
+
