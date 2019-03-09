38546e79fb285e4a1238918e23e41b3ae94d3590
KChenDu <kelvenchendu@gmail.com>
2019-03-09 02:07:28 -0300

    BGM addiction

--- main.py
+++ main.py
@@ -34,9 +34,15 @@ def run_game():
     # Create the fleet of aliens.
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
+    # setting BGM
+    pygame.mixer.init()
+    pygame.mixer.music.load("sound/魂斗罗 归来剧情模式经典6.mp3")
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if pygame.mixer.music.get_busy() == False:
+            pygame.mixer.music.play()
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
