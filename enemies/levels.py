c8040ddaf66fc2a80a33a67f3c25eaaa65577da2
mouradfelipe <felpsmp1999@gmail.com>
2019-04-01 14:44:36 -0300

    testes de distribuicao

---
+++ enemies/levels.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.stats import *
+import math
+import random
+
+class Level:
+    def __init__(self):
+        pass
+
+    def create_fleet(self):
+
+        "codigo distribuicao"
+
+
--- testesMourad.py
+++ testesMourad.py
@@ -1,18 +1,72 @@
 import numpy as np
 from scipy.stats import *
-
+import math
+import random
+"""
 arr = [1, 2, 3]
 
 print(arr)
 
-nu = range(1,6)
-x = nu
-y = chi2(x,nu)
 
-print(y)
 
 
 df = 3
-vals = chi2.pdf([0.001, 0.5, 10], df)
+vals = 1/(0.1+chi2.cdf([0.001, 0.5, 10, 100], df))
 print(vals)
+
+teste = chi2.rvs(df,size = 10)
+print(teste[0])
+
+vals = []
+
+
+qtde = 1000
+k = 1
+
+i = 1
+
+level = []
+
+while len(level) < qtde:
+    level.append(chi2.rvs(i, size = 1))
+    i = i+1
+print(level)
+
+
+while len(vals) < qtde:
+
+    vals.append(math.ceil(1/(0.1+chi2.cdf(chi2.rvs(i,size = 1), i))))
+    i = i+1
+print(vals)
+"""
+
+""""""""""""""""""""
+
+
+"vou querer q a entrada seja um numero aleatorio entre 0 e 90% da cdf"
+qtde = 300
+nivel = 10
+"entradas = (chi2.rvs(nivel, size = qtde))"
+upperlimit = 10 + math.floor(chi2.ppf(0.90,nivel))
+entradas = [random.randrange(1, upperlimit) for _ in range(qtde)]
+dificuldade = []
+i = 0
+while len(dificuldade) < qtde:
+    dificuldade.append(math.ceil(1/(0.1+chi2.cdf(entradas[i], nivel))))
+    i = i+1
+
+"print(dificuldade)"
+
+
+sum = 0
+for cell in dificuldade:
+    sum = sum + cell
+
+
+mean = sum/len(dificuldade)
+print(mean)
+"print(entradas)"
+
+
+
 
