235ee3c25729e519d6af8b6e4535b3338c0c560e
mouradfelipe <felpsmp1999@gmail.com>
2019-04-22 16:27:57 -0300

    modificacoes de definicao de levels

--- enemies/levels.py
+++ enemies/levels.py
@@ -1,7 +1,7 @@
 import numpy as np
 from alien import Alien
 from scipy.stats import *
-from game_functions import *
+import game_functions as gf
 import math
 import random
 
@@ -16,30 +16,31 @@ class Level:
     def __init__(self):
         pass
 
-    @staticmethod
-    def create_fleet(self, ai_settings1, screen1, ship1, aliens1):
+    @classmethod
+    def create_fleet(cls, ai_settings1, screen1, ship1, aliens1):
 
         "codigo distribuicao"
-        ai_settings = ai_settings1
-        screen = screen1
-        ship = ship1
-        aliens = aliens1
+        cls.ai_settings = ai_settings1
+        cls.screen = screen1
+        cls.ship = ship1
+        cls.aliens = aliens1
 
-        alien = Alien(ai_settings, screen)
-        number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
-        number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+        alien = Alien(cls.ai_settings, cls.screen)
+        number_aliens_x = gf.get_number_aliens_x(cls.ai_settings, alien.rect.width)
+        number_rows = gf.get_number_rows(cls.ai_settings, cls.ship.rect.height, alien.rect.height)
         sizefleet = number_rows * number_aliens_x
 
-        nivel = .nivel + 1
+        cls.nivel = cls.nivel + 1
         "entradas = (chi2.rvs(nivel, size = qtde))"
-        upperlimit = 10 + math.floor(chi2.ppf(0.90, self.nivel))
-        entradas = [random.randrange(1, upperlimit) for _ in range(self.sizefleet)]
+        upperlimit = 10 + math.floor(chi2.ppf(0.90, cls.nivel))
+        entradas = [random.randrange(1, upperlimit) for _ in range(sizefleet)]
         dificuldade = []
         i = 0
-        while len(dificuldade) < self.sizefleet:
-            dificuldade.append(math.ceil(1 / (0.1 + chi2.cdf(entradas[i], self.nivel))))
+        while len(dificuldade) < sizefleet:
+            dificuldade.append(math.ceil(1 / (0.1 + chi2.cdf(entradas[i], cls.nivel))))
             i = i + 1
 
-        for row_number in range(self.number_rows):
-            for alien_number in range(self.number_aliens_x):
-                create_alien(self.ai_settings, self.screen, self.aliens, alien_number, row_number)
+        print(dificuldade)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                gf.create_alien(cls.ai_settings, cls.screen, cls.aliens, alien_number, row_number)
